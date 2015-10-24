bce236dfa42354c423e31cac25d299c8733fcb22
Nicholas Peretti <qwertyflagstop@1x-vl925-72-19-105-162.wireless.umass.edu>
2015-10-24 02:08:04 -0400

    inital commit

---
+++ Wopi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wopi
+//
+//  Created by Nicholas Peretti on 10/24/15.
+//  Copyright © 2015 Nicholas Peretti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wopi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wopi
+//
+//  Created by Nicholas Peretti on 10/24/15.
+//  Copyright © 2015 Nicholas Peretti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
