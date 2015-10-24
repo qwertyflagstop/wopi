6be0a2a75de945b9e5fdfa807346842799aa29a2
Nicholas Peretti <nicholas@perettidesign.com>
2015-10-24 03:04:05 -0400

    Some may argue that this is commit adds the EZAudio library, they would be correct

--- Wopi/ViewController.h
+++ Wopi/ViewController.h
@@ -7,8 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "EZAudio.h"
 
 @interface ViewController : UIViewController
+
+
 
 
 @end
--- Wopi/ViewController.m
+++ Wopi/ViewController.m
@@ -14,8 +14,19 @@
 
 @implementation ViewController
 
+-(instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
